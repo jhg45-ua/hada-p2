6c616e3c97192ead3b3b62dc87e2d6beaf9a7a9d
Ximamon <ximamon@gmail.com>
2026-02-20 12:38:33 +0100

    Hola 2.0

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -8,13 +8,6 @@ namespace Hada
 {
     internal class Barco
     {
-        Dictionary<string, int> barcos = new Dictionary<string, int>()
-        {
-            {"Porta-aviões", 5},
-            {"Navio-tanque", 4},
-            {"Cruzador", 3},
-            {"Submarino", 3},
-            {"Destroyer", 2}
-        };
+        Dictionary<Coordenada, String> CoordenadaBarco;
     }
 }
