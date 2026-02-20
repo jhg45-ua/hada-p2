fd1f2ffa0639c267db01795a52a6ff6c55aa0181
jhg45 <jhg45@alu.ua.es>
2026-02-20 12:01:47 +0100

    48795869n clase Coordenada basica

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -8,5 +8,52 @@ namespace Hada
 {
     internal class Coordenada
     {
+        // Atributos
+        public int Fila;
+        public int Columna;
+
+        public Coordenada()
+        {
+            this.Fila = 0;
+            this.Columna = 0;
+        }
+
+        public Coordenada(int fila, int columna)
+        {
+            this.Fila = fila;
+            this.Columna = columna;
+        }
+
+        public Coordenada(string fila, string columna)
+        {
+            this.Fila = int.Parse(fila);
+            this.Columna = int.Parse(columna);
+        }
+
+        public override string ToString()
+        {
+            return "(" + this.Fila + "," + this.Columna + ")";
+        }
+
+        public Coordenada(Coordenada otra)
+        {
+            this.Fila = otra.Fila;
+            this.Columna = otra.Columna;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Coordenada coordenada &&
+                   Fila == coordenada.Fila &&
+                   Columna == coordenada.Columna;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1681403097;
+            hashCode = hashCode * -1521134295 + Fila.GetHashCode();
+            hashCode = hashCode * -1521134295 + Columna.GetHashCode();
+            return hashCode;
+        }
     }
 }
