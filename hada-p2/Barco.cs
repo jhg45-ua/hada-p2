5a20921863a287fed3a7e2a9579b77a0ecabf322
Ximamon <ximamon@gmail.com>
2026-02-20 13:02:33 +0100

    74392890B Comentario del sensei

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Hada
 {
     internal class Barco
-    {
+    { //aqui esta el codigo, estte es mi trabajo, os dejo el resto
         Dictionary<Coordenada, String> CoordenadaBarco;
     }
 }
