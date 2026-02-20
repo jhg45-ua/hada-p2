7e0c13205bba199699a947d1c4676fa469b0853c
jhg45 <jhg45@alu.ua.es>
2026-02-20 12:04:41 +0100

    Test basico de Coordenada en main

--- hada-p2/Program.cs
+++ hada-p2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hada;
 
 namespace Hada
 {
@@ -10,6 +11,9 @@ namespace Hada
     {
         static void Main(string[] args)
         {
+            Coordenada c1 = new Coordenada(1, 2);
+            // Imprime la coordenada c1
+            Console.WriteLine(c1); // Salida: (1,2)
         }
     }
 }
