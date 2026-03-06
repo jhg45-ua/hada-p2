9850a60cb0f2c3d9a0311f1839b68334ce98511b
Ximamon44 <ximamon44@gmail.com>
2026-03-06 11:53:32 +0100

    74392890B Clase Game.cs entera

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -8,5 +8,49 @@ namespace Hada
 {
     public class Game
     {
+        private bool finPartida = false;
+
+        private void gameLoop()
+        {
+            // Generamos primero los tres barcos
+            Barco barco1 = new Barco("Fragata Cristobal Colón", 4, 'v', new Coordenada(1, 1));
+            Barco barco2 = new Barco("Destructor Imperial", 4, 'v', new Coordenada(1, 2));
+            Barco barco3 = new Barco("SES Escudo del Pueblo", 4, 'v', new Coordenada(1, 3));
+
+            Tablero tablero = new Tablero(10, new List<Barco>() { barco1, barco2, barco3 });
+
+            do {
+                Console.WriteLine("Introduce las coordenadas de tu disparo (fila,columna) o 's' para salir:");
+                string input = Console.ReadLine();
+
+                if (string.Equals(input, "s", StringComparison.OrdinalIgnoreCase))
+                {
+                    finPartida = true;
+                    Console.WriteLine("Partida finalizada por el usuario.");
+                    continue;
+                }
+
+                string[] partes = input.Split(',');
+                if (partes.Length != 2)
+                {
+                    Console.WriteLine("Entrada no válida. Por favor, introduce dos números separados por un espacio.");
+                    continue;
+                }
+                if (!int.TryParse(partes[0], out int fila) || !int.TryParse(partes[1], out int columna))
+                {
+                    Console.WriteLine("Entrada no válida. Por favor, introduce números enteros.");
+                    continue;
+                }
+                Coordenada disparo = new Coordenada(fila, columna);
+                tablero.Disparar(disparo);
+                tablero.eventoFinPartida += cuandoEventoFinPartida;
+            } while (!finPartida);
+        }
+
+        private void cuandoEventoFinPartida(object sender, EventArgs e)
+        {
+            Console.WriteLine("PARTIDA FINALIZADA!!");
+            finPartida = true;
+        }
     }
 }
