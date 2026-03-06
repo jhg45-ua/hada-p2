98cceea438613de52cdeacb95bd4d732a9b09fcd
jhg45-ua <jhg45@alu.ua.es>
2026-03-06 11:37:58 +0100

    48795869N Clase tablero completa

--- hada-p2/Barco.cs
+++ hada-p2/Barco.cs
@@ -8,9 +8,9 @@ namespace Hada
 {
     public class Barco
     { //aqui esta el codigo, estte es mi trabajo, os dejo el resto
-        Dictionary<Coordenada, String> CoordenadaBarco;
-        string Nombre;
-        int NumDanyos;
+        public Dictionary<Coordenada, String> CoordenadasBarco;
+        public string Nombre;
+        public int NumDanyos;
 
         public EventHandler<TocadoArgs> eventoTocado;
         public EventHandler<HundidoArgs> eventoHundido;
@@ -19,35 +19,35 @@ namespace Hada
         {
             Nombre = nombre;
 
-            CoordenadaBarco = new Dictionary<Coordenada, string>();
-            CoordenadaBarco.Add(coordenadaInicio, Nombre);
+            CoordenadasBarco = new Dictionary<Coordenada, string>();
+            CoordenadasBarco.Add(coordenadaInicio, Nombre);
 
             if (orientacion == 'h')
             {
                 for (int i = 1; i < longitud; i++)
                 {
-                    CoordenadaBarco.Add(new Coordenada(coordenadaInicio.Fila, coordenadaInicio.Columna + i), Nombre);
+                    CoordenadasBarco.Add(new Coordenada(coordenadaInicio.Fila, coordenadaInicio.Columna + i), Nombre);
                 }
             }
             else if (orientacion == 'v')
             {
                 for (int i = 1; i < longitud; i++)
                 {
-                    CoordenadaBarco.Add(new Coordenada(coordenadaInicio.Fila + i, coordenadaInicio.Columna), Nombre);
+                    CoordenadasBarco.Add(new Coordenada(coordenadaInicio.Fila + i, coordenadaInicio.Columna), Nombre);
                 }
             }
         }
 
-        void Disparo(Coordenada c)
+        public void Disparo(Coordenada c)
         {
-            if (CoordenadaBarco.ContainsKey(c))
+            if (CoordenadasBarco.ContainsKey(c))
             {
-                CoordenadaBarco[c] = c.ToString() + "_T";
+                CoordenadasBarco[c] = c.ToString() + "_T";
                 NumDanyos++;
                 
                 if (eventoTocado != null) // Verificar si hay suscriptores antes de invocar el evento
                 {
-                    eventoTocado(this, new TocadoArgs(CoordenadaBarco[c], c));
+                    eventoTocado(this, new TocadoArgs(CoordenadasBarco[c], c));
                 }
 
             }
@@ -55,9 +55,9 @@ namespace Hada
 
         bool hundido()
         {
-            for (int i = 0; i < CoordenadaBarco.Count; i++)
+            for (int i = 0; i < CoordenadasBarco.Count; i++)
             {
-                if (CoordenadaBarco.ElementAt(i).Value == Nombre)
+                if (CoordenadasBarco.ElementAt(i).Value == Nombre)
                 {
                     return false;
                 }
@@ -72,9 +72,9 @@ namespace Hada
             {
                 String output = "[" + Nombre + "] - Daños :[" + NumDanyos + "] - HUNDIDO: [TRUE] - COORDENADAS: ";
 
-                for (int i = 0; i < CoordenadaBarco.Count; i++)
+                for (int i = 0; i < CoordenadasBarco.Count; i++)
                 {
-                    output += "[" + CoordenadaBarco.ElementAt(i).Key.ToString() + " :" + Nombre + "]";
+                    output += "[" + CoordenadasBarco.ElementAt(i).Key.ToString() + " :" + Nombre + "]";
                 }
 
                 return output;
@@ -83,9 +83,9 @@ namespace Hada
             {
                 String output = "[" + Nombre + "] - Daños :[" + NumDanyos + "] - HUNDIDO: [FALSE] - COORDENADAS: ";
 
-                for (int i = 0; i < CoordenadaBarco.Count; i++)
+                for (int i = 0; i < CoordenadasBarco.Count; i++)
                 {
-                    output += "[" + CoordenadaBarco.ElementAt(i).Key.ToString() + " :" + CoordenadaBarco.ElementAt(i).Value + "]";
+                    output += "[" + CoordenadasBarco.ElementAt(i).Key.ToString() + " :" + CoordenadasBarco.ElementAt(i).Value + "]";
                 }
 
                 return output;
--- hada-p2/Tablero.cs
+++ hada-p2/Tablero.cs
@@ -8,9 +8,11 @@ namespace Hada
 {
     public class Tablero
     {
-        // Atributos
-        public int TamTablero 
-        { 
+        public event EventHandler<EventArgs> eventoFinPartida;
+
+        private int _tamTablero;
+        public int TamTablero
+        {
             get { return _tamTablero; }
             set
             {
@@ -22,15 +24,14 @@ namespace Hada
                 {
                     throw new ArgumentException("El tamaño del tablero debe ser entre 4 y 9.");
                 }
-            } 
+            }
         }
-        private int _tamTablero;
 
         private List<Coordenada> coordenadasDisparadas { get; set; }
         private List<Coordenada> coordenadasTocadas { get; set; }
-        private List<Barco> barcos  { get; set; }
-        private List<Barco> barcosEliminados    { get; set; }
-        private Dictionary<Coordenada, String> casillasTablero  { get; set; }
+        private List<Barco> barcos { get; set; }
+        private List<Barco> barcosEliminados { get; set; }
+        private Dictionary<Coordenada, String> casillasTablero { get; set; }
 
         public Tablero(int tamTablero, List<Barco> barcos)
         {
@@ -43,26 +44,123 @@ namespace Hada
 
             foreach (Barco barco in barcos)
             {
-                barco.eventoTocado += cuandoBarcoTocado;
-                barco.eventoHundido += cuandoBarcoHundido;
+                barco.eventoTocado += cuandoEventoTocado;
+                barco.eventoHundido += cuandoEventoHundido;
             }
 
-            for (int fila = 0; fila < tamTablero; fila++)
+            inicializaCasillasTablero();
+        }
+
+        private void inicializaCasillasTablero()
+        {
+            for (int fila = 0; fila < TamTablero; fila++)
             {
-                for (int columna = 0; columna < tamTablero; columna++)
+                for (int columna = 0; columna < TamTablero; columna++)
                 {
                     casillasTablero.Add(new Coordenada(fila, columna), "AGUA");
                 }
             }
+
+            foreach (Barco barco in barcos)
+            {
+                foreach (Coordenada coord in barco.CoordenadasBarco.Keys)
+                {
+                    casillasTablero[coord] = barco.Nombre;
+                }
+            }
         }
 
-        private void cuandoBarcoTocado(object sender, TocadoArgs e)
+        private void cuandoEventoTocado(object sender, TocadoArgs e)
         {
+            Console.WriteLine($"TABLERO: Barco {e.nombre} tocado en Coordenada: [{e.coordenadaImpacto}]");
 
+            casillasTablero[e.coordenadaImpacto] = e.nombre + "_T";
+
+            if (!coordenadasTocadas.Contains(e.coordenadaImpacto))
+            {
+                coordenadasTocadas.Add(e.coordenadaImpacto);
+            }
         }
 
-        private void cuandoBarcoHundido(object sender, HundidoArgs e)
+        private void cuandoEventoHundido(object sender, HundidoArgs e)
         {
+            Console.WriteLine($"TABLERO: Barco {e.nombre} hundido!!");
+
+            Barco barcoHundido = barcos.FirstOrDefault(b => b.Nombre == e.nombre);
+            if (barcoHundido != null && !barcosEliminados.Contains(barcoHundido))
+            {
+                barcosEliminados.Add(barcoHundido);
+            }
+
+            if (barcosEliminados.Count == barcos.Count)
+            {
+                if (eventoFinPartida != null)
+                {
+                    eventoFinPartida(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public void Disparar(Coordenada c)
+        {
+            if (c.Fila < 0 || c.Fila >= TamTablero || c.Columna < 0 || c.Columna >= TamTablero)
+            {
+                Console.WriteLine($"La coordenada ({c.Fila},{c.Columna}) está fuera de las dimensiones del tablero.");
+                return;
+            }
+
+            coordenadasDisparadas.Add(c);
+
+            foreach (Barco barco in barcos)
+            {
+                barco.Disparo(c);
+            }
+        }
+
+        public string DibujarTablero()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("CASILLAS TABLERO");
+            sb.AppendLine("--------");
+
+            for (int fila = 0; fila < TamTablero; fila++)
+            {
+                for (int columna = 0; columna < TamTablero; columna++)
+                {
+                    Coordenada actual = new Coordenada(fila, columna);
+                    sb.Append($"[{casillasTablero[actual]}]");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Barco barco in barcos)
+            {
+                sb.AppendLine(barco.ToString());
+            }
+
+            sb.Append("Coordenadas disparadas: ");
+            foreach (Coordenada c in coordenadasDisparadas)
+            {
+                sb.Append(c.ToString() + " ");
+            }
+            sb.AppendLine();
+
+            sb.Append("Coordenadas tocadas: ");
+            foreach (Coordenada c in coordenadasTocadas)
+            {
+                sb.Append(c.ToString() + " ");
+            }
+            sb.AppendLine("\n");
+
+            sb.Append(DibujarTablero());
+
+            return sb.ToString();
         }
     }
 }
